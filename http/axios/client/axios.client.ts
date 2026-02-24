deb33bf2b89f28227be50bbf85e9833976532f9b
Samuel Sanderson <sandersonsamuel401@gmail.com>
2026-02-24 12:29:47 -0300

    refactor: proxy API requests through Next.js rewrites and update Axios base URLs.

--- http/axios/client/axios.client.ts
+++ http/axios/client/axios.client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 export const axiosClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: "/api",
     withCredentials: true,
 })
 
--- http/axios/server/axios.server.ts
+++ http/axios/server/axios.server.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { cookies } from 'next/headers';
 
 export const axiosServer = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: "/api",
 });
 
 axiosServer.interceptors.request.use(async (config) => {
